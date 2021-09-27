d6e099f50e6ad6a06b454d8e8ffaed409f0e1ff6
s3fs <northfacebodybags@yahoo.com>
2021-09-27 20:54:17 +0500

    polyfills

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import 'core-js/stable/index.js'
 import 'regenerator-runtime/runtime.js'
+import PromisePolyfill from 'promise-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from "./App.js"
+
+//polyfills: https://github.com/Modernizr/Modernizr/wiki/HTML5-Cross-browser-Polyfills
+if(!window.Promise) {
+  window.Promise = PromisePolyfill
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
